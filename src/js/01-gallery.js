120d909d692362e3537b73fd81181cb64f862787
valentine-e <valia101112@gmail.com>
2022-05-25 16:52:02 +0300

    1-2-done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryWrapper = document.querySelector('.gallery');
+const galleryEl = createGallery(galleryItems);
+galleryWrapper.innerHTML = galleryEl;
+
+new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  fadeSpeed: 250,
+  showCounter: false,
+});
+
+function createGallery(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+// console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeEl = document.querySelector('iframe');
+const player = new Player(iframeEl);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const onPlay = function (data) {
+  localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const savedData = localStorage.getItem(LOCALSTORAGE_KEY);
+
+player.setCurrentTime(savedData).then(function (seconds) {
+  seconds = this.setCurrentTime;
+});
